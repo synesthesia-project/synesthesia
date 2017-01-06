c9706946dc3bc25d8a45eee6ec3de700260490b7
Sam Lanning <sam@samlanning.com>
2017-01-06 08:29:48 -0800

    further styling experiments

--- scripts/ts/components/stage.tsx
+++ scripts/ts/components/stage.tsx
@@ -25,18 +25,13 @@ export class Stage extends React.Component<StageProps, StageState> {
   }
 
   render() {
-    const style = `
-      h1 {
-        color: green;
-      }
-    `;
-
     return (
       <externals.ShadowDOM>
         <div>
           <link rel="stylesheet" type="text/css" href="dist/styles/components/stage.css"/>
-          <style type="text/css">{style}</style>
-          <h1>Hello!</h1>
+          <div id="main">
+
+          </div>
           <Player />
         </div>
       </externals.ShadowDOM>
