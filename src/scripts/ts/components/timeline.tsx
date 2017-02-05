008ee120a6e25045d715926130f2bb12bb5bfa16
Sam Lanning <sam@samlanning.com>
2017-02-05 13:20:31 -0800

    Add "add layer" button

--- src/scripts/ts/components/timeline.tsx
+++ src/scripts/ts/components/timeline.tsx
@@ -16,7 +16,9 @@ export class Timeline extends BaseComponent<TimelineProps, TimelineState> {
       <externals.ShadowDOM>
         <div>
           <link rel="stylesheet" type="text/css" href="dist/styles/components/timeline.css"/>
-          <div className="side"></div>
+          <div className="side">
+            <span className="add-button"/>
+          </div>
         </div>
       </externals.ShadowDOM>
     );
