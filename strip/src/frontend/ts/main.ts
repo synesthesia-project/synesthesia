f3298e44082df63d677c37682663fed2372034a0
Sam Lanning <sam@samlanning.com>
2017-04-25 23:52:36 -0700

    strip: change some values from browser form elements

--- strip/src/frontend/ts/main.ts
+++ strip/src/frontend/ts/main.ts
@@ -2,13 +2,41 @@
 
   var socket = new WebSocket("ws://localhost:8120/strip");
 
+  const formElements = {
+    primaryArtifacts: $('#primaryArtifacts'),
+    secondaryArtifacts: $('#secondaryArtifacts'),
+    sparkliness: $('#sparkliness')
+  };
+
   socket.onmessage = event => {
     const msg: synesthesia.strip_controller_api.ServerMessage = JSON.parse(event.data);
     console.log('event', msg);
     if (msg.state) {
+      formElements.primaryArtifacts.val(msg.state.primaryArtifacts);
+      formElements.secondaryArtifacts.val(msg.state.secondaryArtifacts);
+      formElements.sparkliness.val(msg.state.sparkliness);
     }
   }
 
+  const updateState = (state: Partial<synesthesia.strip_controller_api.StripState>) => {
+    const msg: synesthesia.strip_controller_api.ClientMessage = {
+      updateState: state
+    };
+    socket.send(JSON.stringify(msg));
+  }
+
+  // Add listeners to Dom
+  formElements.primaryArtifacts.on('change', () =>
+    updateState({primaryArtifacts: formElements.primaryArtifacts.val()})
+  );
+  formElements.secondaryArtifacts.on('change', () =>
+    updateState({secondaryArtifacts: formElements.secondaryArtifacts.val()})
+  );
+  formElements.sparkliness.on('change', () =>
+    updateState({sparkliness: formElements.sparkliness.val()})
+  );
+
+  // Expose Update State via window
   this.window.updateState = (msg: synesthesia.strip_controller_api.ClientMessage) => socket.send(JSON.stringify(msg));
 
 })();
