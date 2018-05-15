51e5131065ec01faf6105813960e6e2476216bef
Sam Lanning <sam@samlanning.com>
2018-05-14 21:29:05 -0700

    Convert PlayerBar to StyledComponent

--- composer/src/scripts/ts/components/player-bar.tsx
+++ composer/src/scripts/ts/components/player-bar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {styled} from './styling';
 
 import * as util from '../shared/util/util';
 
@@ -24,7 +25,7 @@ export interface PlayerBarProps {
   updateScrubbingPosition: (position: func.Maybe<number>) => void;
 }
 
-export class PlayerBar extends React.Component<PlayerBarProps, PlayerBarState> {
+class PlayerBar extends React.Component<PlayerBarProps, PlayerBarState> {
 
   private updateInterval: number;
 
@@ -66,26 +67,23 @@ export class PlayerBar extends React.Component<PlayerBarProps, PlayerBarState> {
     });
     const buttonLeft = (util.restrict(buttonPosition, 0, 1) * 100) + '%';
     return (
-      <externals.ShadowDOM>
-        <div className={className}>
-          <link rel="stylesheet" type="text/css" href="styles/components/player-bar.css"/>
-          <div className="zoom" style={{
-            left: this.props.zoom.startPoint * 100 + '%',
-            right: (1 - this.props.zoom.endPoint) * 100 + '%'
-            }}>
-          </div>
-          <div className="bar" ref={bar => this.barRef = bar}>
-            <div className="fill" style={{width: fillWidth}} />
-          </div>
-          <div className="button" style={{left: buttonLeft}} />
-          <div className="hit"
-            onMouseDown={this.onMouseDown}
-            onMouseMove={this.onMouseMove}
-            onMouseUp={this.onMouseUp}
-            onMouseOut={this.onMouseUp}
-          ></div>
+      <div className={className}>
+        <div className="zoom" style={{
+          left: this.props.zoom.startPoint * 100 + '%',
+          right: (1 - this.props.zoom.endPoint) * 100 + '%'
+          }}>
         </div>
-      </externals.ShadowDOM>
+        <div className="bar" ref={bar => this.barRef = bar}>
+          <div className="fill" style={{width: fillWidth}} />
+        </div>
+        <div className="button" style={{left: buttonLeft}} />
+        <div className="hit"
+          onMouseDown={this.onMouseDown}
+          onMouseMove={this.onMouseMove}
+          onMouseUp={this.onMouseUp}
+          onMouseOut={this.onMouseUp}
+        ></div>
+      </div>
     );
   }
 
@@ -162,3 +160,125 @@ export class PlayerBar extends React.Component<PlayerBarProps, PlayerBarState> {
   }
 
 }
+
+const buttonHeightPx = 16;
+const buttonWidthPx = 6;
+const buttonBorderPx = 1;
+const barWidthPx = 2;
+const barBorderPx = 1;
+
+const StyledPlayerBar = styled(PlayerBar)`
+  position: relative;
+  flex-grow: 1;
+  height: ${buttonHeightPx}px;
+  margin: 0 ${buttonWidthPx / 2}px;
+  cursor: pointer;
+
+  .bar {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: ${(buttonHeightPx - barWidthPx - barBorderPx * 2) / 2}px;
+    height: ${barWidthPx}px;
+    background-color: ${p => p.theme.borderDark};
+    border: 1px solid ${p => p.theme.borderDark};
+
+    .fill {
+      background-color: ${p => p.theme.borderLight};
+      position: absolute;
+      height: 100%;
+      width: 50%;
+    }
+  }
+
+  .button {
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin-left: ${-buttonWidthPx / 2 - buttonBorderPx}px;
+    width: ${buttonWidthPx}px;
+    height: ${buttonHeightPx}px;
+    border-radius: ${buttonHeightPx}px;
+    border: ${buttonBorderPx}px solid ${p => p.theme.borderDark};
+    background: ${p => p.theme.bg};
+  }
+
+  // Increase hit-area
+  .hit {
+    position: absolute;
+    top: -5px;
+    right: -15px;
+    bottom: -5px;
+    left: -15px;
+  }
+
+  .zoom {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    border-right: 1px solid ${p => p.theme.borderDark};
+    border-left: 1px solid ${p => p.theme.borderDark};
+    background: ${p => p.theme.bg};
+  }
+
+  &:hover {
+    .bar {
+      border-color: ${p => p.theme.hintDark1};
+
+      .fill {
+        background: ${p => p.theme.hintDark1};
+      }
+    }
+
+    .button {
+      border-color: ${p => p.theme.hintDark1};
+    }
+  }
+
+  &.dragging {
+    .bar {
+      border-color: ${p => p.theme.hint};
+
+      .fill {
+        background: ${p => p.theme.hint};
+      }
+    }
+
+    .button {
+      border-color: ${p => p.theme.hint};
+      background: ${p => p.theme.bg};
+    }
+
+    // Expand hit area to whole screen while dragging
+    .hit {
+      top: -100vh;
+      right: -10vw;
+      left: -10vw;
+      bottom: -50px;
+    }
+  }
+
+  &.disabled {
+    cursor: default;
+
+    .bar {
+      background-color: ${p => p.theme.bg};
+      border-color: ${p => p.theme.bg};
+
+      .fill {
+        background-color: ${p => p.theme.bg};
+      }
+    }
+
+    .button {
+      display: none;
+    }
+
+    .zoom {
+      border-color: ${p => p.theme.bg};
+    }
+  }
+`;
+
+export {StyledPlayerBar as PlayerBar};
