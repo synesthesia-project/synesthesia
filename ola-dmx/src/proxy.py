ced197abfc35c5e6b32d8048aa15dd2940c62baa
Sam Lanning <sam@samlanning.com>
2018-11-02 20:19:04 -0700

    Support both python3 and python2 for proxy

--- ola-dmx/src/proxy.py
+++ ola-dmx/src/proxy.py
@@ -13,6 +13,11 @@ universe = 0
 wrapper = ClientWrapper()
 client = wrapper.Client()
 
-while True:
-    bytes = bytearray(sys.stdin.buffer.read(UNIVERSE_SIZE + 1))
-    client.SendDmx(bytes[0], array.array('B', bytes[1:]), DmxSent)
+if sys.version_info[0] < 3:
+    while True:
+        bytes = bytearray(sys.stdin.read(UNIVERSE_SIZE + 1))
+        client.SendDmx(bytes[0], array.array('B', bytes[1:]), DmxSent)
+else:
+    while True:
+        bytes = bytearray(sys.stdin.buffer.read(UNIVERSE_SIZE + 1))
+        client.SendDmx(bytes[0], array.array('B', bytes[1:]), DmxSent)
