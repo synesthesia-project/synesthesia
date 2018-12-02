92a3253b02232d76875f1da60ca2f9cf33bcd4a5
Sam Lanning <sam@samlanning.com>
2018-12-02 13:34:01 -0800

    home-control: introduce project

---
+++ home-control/README.md
@@ -0,0 +1,4 @@
+# Personal project for controlling lighting at home
+
+A personal project pulling together other elements of synesthesia, light-desk
+and other lighting stuff to control lighting at home.
---
+++ home-control/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var ts = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+var runSequence = require('run-sequence');
+
+var tsProject = ts.createProject('src/tsconfig.json');
+
+// Utility Functions
+
+function handleError(err) {
+  gutil.log("Build failed", err.message);
+  process.exit(1);
+}
+
+gulp.task('clean', function() {
+  return gulp.src(['build'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('ts', function () {
+    return tsProject.src()
+      .pipe(tsProject())
+      .pipe(gulp.dest('build/'));
+});
+
+gulp.task('tslint', function() {
+  return gulp.src(['src/**/*.ts'])
+  .pipe(tslint({
+    formatter: 'verbose',
+    configuration: '../tslint.json'
+  }))
+  .on('error', handleError)
+  .pipe(tslint.report());
+});
+
+gulp.task('default', function(callback) {
+  runSequence(
+    'clean',
+    'ts',
+    'tslint',
+    callback);
+});
