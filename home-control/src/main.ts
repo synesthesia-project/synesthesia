7506f92fee6c530e5310d4cd5274fbb22bdaf056
Sam Lanning <sam@samlanning.com>
2018-12-02 13:36:08 -0800

    home-control: Add light desk as dependency

--- home-control/src/main.ts
+++ home-control/src/main.ts
@@ -1,1 +1,6 @@
+import * as lightDesk from '@samlanning/synesthesia-light-desk';
+
 console.log('heeelooo');
+
+const desk = new lightDesk.LightDesk();
+desk.setRoot(new lightDesk.Group({direction: 'vertical'}));
