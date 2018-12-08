b64963c26782e9587e324ac30088f2b92ac835cb
Sam Lanning <sam@samlanning.com>
2018-12-08 10:35:16 -0800

    ola-dmx: Make proxy script more robust to olad connection issues

--- ola-dmx/src/proxy.py
+++ ola-dmx/src/proxy.py
@@ -1,23 +1,35 @@
 import array
 import sys
+import time
 
 UNIVERSE_SIZE = 512
 
 from ola.ClientWrapper import ClientWrapper
+from ola.OlaClient import OLADNotRunningException
 
 def DmxSent(state):
   pass
 
 universe = 0
 
-wrapper = ClientWrapper()
-client = wrapper.Client()
+client = None
 
-if sys.version_info[0] < 3:
-    while True:
+while True:
+    if client is None:
+        try:
+            client = ClientWrapper().Client()
+        except OLADNotRunningException as e:
+            print('olad not running, waiting until ready')
+            time.sleep(1)
+            continue
+    bytes = None
+    if sys.version_info[0] < 3:
         bytes = bytearray(sys.stdin.read(UNIVERSE_SIZE + 1))
-        client.SendDmx(bytes[0], array.array('B', bytes[1:]), DmxSent)
-else:
-    while True:
+    else:
         bytes = bytearray(sys.stdin.buffer.read(UNIVERSE_SIZE + 1))
+    try:
         client.SendDmx(bytes[0], array.array('B', bytes[1:]), DmxSent)
+    except OLADNotRunningException as e:
+        print('olad has shut down')
+        client = None
+        pass
