046522da039124b2a7bb760ee96a129738d14765
Sam Lanning <sam@samlanning.com>
2018-12-07 23:23:09 -0800

    home-control: Control various rooms for hue from control panel.

--- home-control/src/behaviour.ts
+++ home-control/src/behaviour.ts
@@ -1,18 +1,28 @@
 import * as lightDesk from '@samlanning/synesthesia-light-desk';
 import * as ola from '@samlanning/synesthesia-ola-dmx';
-import {HueApi} from 'node-hue-api';
+import {HueApi, lightState} from 'node-hue-api';
 
 const INTERVAL = 1000;
+
+interface StateGroup {
+  id: string;
+  name: string;
+  lights: string[];
+  state: 'unknown' | 'on' | 'off';
+}
 
 interface State {
   externalLignts: {
     state: 'unknown' | 'on' | 'off';
     brightness: number;
   };
+  groups: StateGroup[];
 }
 
 interface Desk {
   externalLightsState: lightDesk.Label;
+  lightGroupsArea: lightDesk.Group;
+  lightGroupToLabel: Map<string, lightDesk.Label>;
 }
 
 export class Behaviour {
@@ -24,7 +34,8 @@ export class Behaviour {
     externalLignts: {
       state: 'unknown',
       brightness: 1
-    }
+    },
+    groups: []
   };
 
   private desk: null | Desk = null;
@@ -43,29 +54,68 @@ export class Behaviour {
     this.display.run();
 
     setInterval(this.interval, INTERVAL);
+
+    // Load rooms
+    this.hue.groups().then(groups => {
+      if (this.state.groups.length > 0) return;
+      for (const group of groups) {
+        if (!group.lights) continue;
+        this.state.groups.push({
+          id: group.id,
+          name: group.name,
+          lights: group.lights,
+          state: 'unknown'
+        });
+      }
+      if (this.desk) {
+        this.addGroupDeskStuff(this.desk, this.state.groups);
+      }
+    });
+  }
+
+  private addGroupDeskStuff(desk: Desk, lightGroups: StateGroup[]) {
+    for (const lightGroup of lightGroups) {
+      const group = new lightDesk.Group({direction: 'horizontal'});
+      const label = new lightDesk.Label(lightGroup.name);
+      desk.lightGroupToLabel.set(lightGroup.id, label);
+      group.addChild(label);
+
+      const button = new lightDesk.Button('Toggle');
+      button.addListener(() => {
+        this.hue.setGroupLightState(lightGroup.id, lightState.create().on(lightGroup.state !== 'on'));
+      });
+      group.addChild(button);
+      desk.lightGroupsArea.addChild(group);
+    }
   }
 
   public getLightDesk() {
 
     const desk = new lightDesk.Group({direction: 'vertical'});
 
-    const homeControl = new lightDesk.Group({direction: 'horizontal'});
+    const homeControl = new lightDesk.Group({direction: 'vertical'});
     desk.addChild(homeControl);
     desk.addChild(this.display.getLightDesk());
 
     this.desk = {
-      externalLightsState: new lightDesk.Label('External Lights:')
+      externalLightsState: new lightDesk.Label('External Lights:'),
+      lightGroupsArea: new lightDesk.Group({direction: 'vertical'}),
+      lightGroupToLabel: new Map()
     };
 
     homeControl.addChild(this.desk.externalLightsState);
+    homeControl.addChild(this.desk.lightGroupsArea);
+    this.addGroupDeskStuff(this.desk, this.state.groups);
 
     return desk;
   }
 
   private async interval() {
     const lights = await this.hue.lights();
-
+    const states = new Map<string, 'on'|'off'>();
     for (const light of lights.lights) {
+      if (!light.id) continue;
+      states.set(light.id, light.state.on ? 'on' : 'off');
       if (light.name === 'Downstairs Living Room 1') {
         const newState = light.state.on ? 'on' : 'off';
         if (this.state.externalLignts.state !== newState) {
@@ -78,6 +128,19 @@ export class Behaviour {
           } else {
             this.display.setBlackout(true);
           }
+        }
+      }
+    }
+    for (const group of this.state.groups) {
+      let newState: 'on' | 'off' = 'off';
+      for (const l of group.lights) {
+        if (states.get(l) === 'on') newState = 'on';
+      }
+      if (group.state !== newState) {
+        group.state = newState;
+        if (this.desk) {
+          const label = this.desk.lightGroupToLabel.get(group.id);
+          if (label) label.setText(`${group.name}: ${newState}`);
         }
       }
     }
