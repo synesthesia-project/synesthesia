8a4a59c5b32ea356cb29bb7c1aab27ea10e84d9f
Sam Lanning <sam@samlanning.com>
2019-06-22 14:11:01 -0700

    Use new pixelmap data from openrazer

--- synesthesia-razer/src/index.ts
+++ synesthesia-razer/src/index.ts
@@ -19,7 +19,15 @@ export class Display {
     files: new Map()
   };
 
-  private keyboard: openrazer.Keyboard | null = null;
+  private keyboard: {
+    keyboard: openrazer.Keyboard,
+    map: openrazer.KeyboardPixelMap,
+    coords: {
+      xMin: number,
+      xMax: number,
+    }
+  } | null = null;
+  private x = 0;
 
   public constructor() {
     this.frame = this.frame.bind(this);
@@ -77,32 +85,81 @@ export class Display {
     setInterval(this.frame, 20);
 
     const keyboards = await openrazer.getKeyboards();
-    if (keyboards.length > 0) this.keyboard = keyboards[0];
+    if (keyboards.length > 0) {
+      const keyboard = keyboards[0];
+      const map = await keyboard.getPixelMap();
+      if (!map) {
+        throw new Error('keyboard has no pixel map');
+      }
+      // calculate xMin and xMax
+      const coords = {xMax: -Infinity, xMin: Infinity};
+      for (const row of map.rows) {
+        for (const key of row.keys) {
+          coords.xMax = Math.max(coords.xMax, key.centreX);
+          coords.xMin = Math.min(coords.xMin, key.centreX);
+        }
+      }
+      this.keyboard = { keyboard, map, coords };
+      this.x = coords.xMin - 20;
+    }
   }
 
-  private frame() {
+  private async frame() {
 
-    const timestampMillis = new Date().getTime();
+    // const timestampMillis = new Date().getTime();
 
     if (this.keyboard) {
-      let amplitude = 0;
-      if (this.state.playState.layers.length === 0) {
-        this.keyboard.setMatrixBrightness(255);
-      } else {
-        for (const layer of this.state.playState.layers) {
-          const f = this.state.files.get(layer.fileHash);
-          if (!f) continue;
-          const t = (timestampMillis - layer.effectiveStartTimeMillis) * layer.playSpeed;
-          for (const fLayer of f.layers) {
-            const activeEvents = usage.getActiveEvents(fLayer.events, t);
-            for (const e of activeEvents) {
-              const a = usage.getCurrentEventStateValue(e, t, state => state.amplitude);
-              amplitude = Math.max(amplitude, a);
-            }
-          }
-        }
-        this.keyboard.setMatrixBrightness(10 + amplitude * 205);
+      const rows = this.keyboard.map.rows;
+      this.x += 10;
+      if ( this.x >= this.keyboard.coords.xMax + 100) {
+        this.x = this.keyboard.coords.xMin - 100;
       }
+      console.log(this.x);
+      const rowData: { index: number, start: number, colors: openrazer.RGB[]}[] = [];
+      for (let r = 0; r < rows.length; r++) {
+        const row = rows[r];
+        const indexes = row.keys.map(k => k.i);
+        const kMin = Math.min(...indexes);
+        const kMax = Math.max(...indexes);
+        const colors: openrazer.RGB[] = [];
+        for (let k = kMin; k <= kMax; k++) {
+          colors[k - kMin] = [0, 0, 0];
+        }
+        // Set brightness of keys based on bar location
+        for (const key of row.keys) {
+          const distance = Math.abs(this.x - key.centreX);
+          const brightness = Math.max(0, 1 - distance / 50);
+          colors[key.i - kMin] = [
+            Math.round(255 * brightness),
+            0,
+            Math.round(255 * brightness),
+          ];
+        }
+        rowData.push({
+          index: r,
+          start: kMin,
+          colors
+        });
+      }
+      await this.keyboard.keyboard.writeCustomFrame(rowData);
+      // let amplitude = 0;
+      // if (this.state.playState.layers.length === 0) {
+      //   this.keyboard.setMatrixBrightness(255);
+      // } else {
+      //   for (const layer of this.state.playState.layers) {
+      //     const f = this.state.files.get(layer.fileHash);
+      //     if (!f) continue;
+      //     const t = (timestampMillis - layer.effectiveStartTimeMillis) * layer.playSpeed;
+      //     for (const fLayer of f.layers) {
+      //       const activeEvents = usage.getActiveEvents(fLayer.events, t);
+      //       for (const e of activeEvents) {
+      //         const a = usage.getCurrentEventStateValue(e, t, state => state.amplitude);
+      //         amplitude = Math.max(amplitude, a);
+      //       }
+      //     }
+      //   }
+      //   this.keyboard.setMatrixBrightness(10 + amplitude * 205);
+      // }
     }
   }
 }
