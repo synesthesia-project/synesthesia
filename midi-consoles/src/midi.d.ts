d7562641c0f3f93d8e6dd668ea1eb3d4375a0746
Sam Lanning <sam@lanni.ng>
2019-07-23 20:15:31 -0700

    Add initial midi decl

---
+++ midi-consoles/src/midi.d.ts
@@ -0,0 +1,33 @@
+declare module 'midi' {
+  import { Stream } from 'stream';
+  class Input {
+    public getPortCount(): number;
+    public getPortName(port: number): string;
+    public on(event: 'message', callback: (deltaTime: number, message: number[]) => void): void;
+    public openPort(port: number): void;
+    public openVirtualPort(name: string): void;
+    /**
+     * sysex, timing, and active sensing messages are ignored
+     * by default. To enable these message types, pass false for
+     * the appropriate type into this function.
+     * 
+     * For example if you want to receive only MIDI Clock beats
+     * you should use:
+     * 
+     * @example input.ignoreTypes(true, false, true)
+     */
+    public ignoreTypes(sysex: boolean, timing: boolean, activeSensing: boolean): void;
+    public closePort(): void;
+  }
+
+  class Output {
+    public getPortCount(): number;
+    public getPortName(port: number): string;
+    public openPort(port: number): void;
+    public sendMessage(message: number[]): void;
+    public closePort(): void;
+  }
+
+  function createReadStream(input: Input): Stream;
+  function createWriteStream(output: Output): Stream;
+}
