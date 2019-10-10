8ff6ab2bb9c5c3fa7d6d9b9e2ace2a214ae8ca61
Sam Lanning <sam@lanni.ng>
2019-10-09 22:18:09 -0700

    Remove bower in composer

--- composer/src/integration/index.ts
+++ composer/src/integration/index.ts
@@ -12,7 +12,6 @@ const DIST = path.dirname(__dirname);
 console.log(DIST);
 
 export const STATIC_FILES: { url: string, path: string, contentType: string }[] = [
-    { url: '/lib/jquery.min.js', path: path.join(DIST, 'lib/jquery.min.js'), contentType: 'text/javascript' },
     { url: '/bundle.js', path: path.join(DIST, 'bundle.js'), contentType: 'text/javascript' },
     { url: '/styles/main.css', path: path.join(DIST, 'styles/main.css'), contentType: 'text/css' }
  ];
@@ -23,7 +22,6 @@ export function getIndexHtml(settings: IntegrationSettings) {
     <html>
     <head>
         <title>Synesthesia Composer</title>
-        <script src="lib/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="styles/main.css" />
     </head>
     <body>
--- composer/src/scripts/ts/components/layer-items.tsx
+++ composer/src/scripts/ts/components/layer-items.tsx
@@ -1,3 +1,4 @@
+import * as jQuery from 'jquery';
 import {styled} from './styling';
 import * as React from 'react';
 import * as file from '@synesthesia-project/core/lib/file';
@@ -254,7 +255,7 @@ class LayerItems extends React.Component<LayerItemsProps, LayerItemsState> {
     return (
       <div className={this.props.className}>
         <div className={'timeline-selector ' + (this.state.selector.state === 'dragging' ? ' dragging' : '')}
-          ref={div => this.timelineSelector = div ? $(div) : null}
+          ref={div => this.timelineSelector = div ? jQuery(div) : null}
           onMouseEnter={this.onTimelineSelectorMouseOver}
           onMouseLeave={this.onTimelineSelectorMouseOut}
           onMouseMove={this.onTimelineSelectorMouseMove}
