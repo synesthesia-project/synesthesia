522be407376299d635af1b4ac55b2ac6be1ec5f3
Sam Lanning <sam@lanni.ng>
2019-10-09 20:48:18 -0700

    Remove strip and home-control from repo

--- home-control/README.md
+++
@@ -1,4 +0,0 @@
-# Personal project for controlling lighting at home
-
-A personal project pulling together other elements of synesthesia, light-desk
-and other lighting stuff to control lighting at home.
--- strip/src/ts/global.imports.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../node_modules/@types/node/index.d.ts"/>
-/// <reference path="../../node_modules/@types/ws/index.d.ts"/>
-/// <reference path="../shared/ts/strip-controller-api.d.ts"/>
