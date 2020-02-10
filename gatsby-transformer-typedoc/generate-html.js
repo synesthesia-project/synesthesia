9e82bd28c66074795f7a6684ae147466d5015497
Sam Lanning <sam@samlanning.com>
2020-02-10 15:56:50 +0000

    Use React SSR to generate docs

--- gatsby-transformer-typedoc/generate-html.js
+++ gatsby-transformer-typedoc/generate-html.js
@@ -1,27 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const server_1 = require("react-dom/server");
 const abstract_1 = require("typedoc/dist/lib/models/reflections/abstract");
 function generatePageHTML(pages, _sectionMap, page) {
-    const html = [];
-    const decendantModules = [];
+    const components = [];
+    const descendantModules = [];
     for (const p of pages.values()) {
         if (p !== page &&
             p.url.startsWith(page.url) &&
             p.sections.length === 1 &&
             p.sections[0].reflection.kind === abstract_1.ReflectionKind.ExternalModule)
-            decendantModules.push(p);
+            descendantModules.push(p);
     }
-    if (decendantModules.length > 0) {
-        html.push(`
-    <h2>Decendant Modules:</h2>
-    <ul>
-      ${decendantModules.map(m => `
-      <li>${m.sections[0].title}</li>
-      `)}
-    </ul>
-    `);
+    if (descendantModules.length > 0) {
+        components.push(React.createElement("div", { key: 'descendantModules' },
+            React.createElement("h2", null, "Descendent Modules"),
+            React.createElement("ul", null, descendantModules.map(m => (React.createElement("li", { key: m.url },
+                "Foo: ",
+                m.sections[0].title))))));
     }
-    console.log(decendantModules);
-    return html.join();
+    return server_1.renderToString(React.createElement(React.Fragment, null, components));
 }
 exports.generatePageHTML = generatePageHTML;
--- gatsby-transformer-typedoc/src/generate-html.tsx
+++ gatsby-transformer-typedoc/src/generate-html.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
 import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract';
 import { InitialDocumentationPage, DocumentationSection } from './process-typedoc';
 
@@ -5,32 +7,27 @@ export function generatePageHTML(
   pages: Map<string, InitialDocumentationPage>,
   _sectionMap: Map<number, DocumentationSection>,
   page: InitialDocumentationPage) {
-  const html: string[] = [];
+  const components: JSX.Element[] = [];
 
   // Check to see if there are any modules that share the same URL prefix
-  const decendantModules: InitialDocumentationPage[] = [];
+  const descendantModules: InitialDocumentationPage[] = [];
   for (const p of pages.values()) {
     if (p !== page &&
         p.url.startsWith(page.url) &&
         p.sections.length === 1 &&
         p.sections[0].reflection.kind === ReflectionKind.ExternalModule)
-      decendantModules.push(p);
+      descendantModules.push(p);
   }
-  if (decendantModules.length > 0 ) {
-    html.push(`
-    <h2>Decendant Modules:</h2>
-    <ul>
-      ${decendantModules.map(m => `
-      <li>${m.sections[0].title}</li>
-      `)}
-    </ul>
-    `)
-  }
-  console.log(decendantModules);
+  if (descendantModules.length > 0 ) {
+    components.push(<div key='descendantModules'>
+      <h2>Descendent Modules</h2>
+      <ul>
+        {descendantModules.map(m => (
+          <li key={m.url}>Foo: {m.sections[0].title}</li>
+        ))}
+      </ul>
+    </div>);
+  }  
 
-
-
-  
-
-  return html.join();
+  return renderToString(<>{components}</>);
 }
