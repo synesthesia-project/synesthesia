9ede0d4e7e216a0825624fc3aabbe49c0cbab6ea
Sam Lanning <sam@samlanning.com>
2020-03-12 15:44:42 +0000

    gapless-meta: Add some CBR tests

--- gapless-meta/src/tests/index.test.ts
+++ gapless-meta/src/tests/index.test.ts
@@ -10,25 +10,69 @@ const TRACKS_SINTEL = path.join(TRACKS, 'sintel');
 
 describe('index.ts', () => {
 
-  it('getMetadata', async () => {
-    const src = path.join(TRACKS_SINTEL, 'sintel_1.mp3');
-    const metadata = await getMetadataFromFile(src);
-    expect(metadata).to.deep.equal({
-      version: '1',
-      layer: '3',
-      bitrate: 128,
-      mode: 'joint_stereo',
-      sampleRate: 44100,
-      samplesPerFrame: 1152,
-      vbrInfo: {
-        isCBR: false,
-        numberOfFrames: 249,
-      },
-      lameInfo: {
-        encoder: 'LAME3.99r',
-        paddingStart: 576,
-        paddingEnd: 576
-      },
+  describe('getMetadata', () => {
+
+    it('sintel_1.mp3', async () => {
+      const src = path.join(TRACKS_SINTEL, 'sintel_1.mp3');
+      expect(await getMetadataFromFile(src)).to.deep.equal({
+        version: '1',
+        layer: '3',
+        bitrate: 128,
+        mode: 'joint_stereo',
+        sampleRate: 44100,
+        samplesPerFrame: 1152,
+        vbrInfo: {
+          isCBR: false,
+          numberOfFrames: 249,
+        },
+        lameInfo: {
+          encoder: 'LAME3.99r',
+          paddingStart: 576,
+          paddingEnd: 576
+        },
+      });
+    });
+
+    it('sintel-clip-lame-cbr-128.mp3', async () => {
+      const src = path.join(TRACKS_SINTEL, 'sintel-clip-lame-cbr-128.mp3');
+      expect(await getMetadataFromFile(src)).to.deep.equal({
+        version: '1',
+        layer: '3',
+        bitrate: 128,
+        mode: 'joint_stereo',
+        sampleRate: 44100,
+        samplesPerFrame: 1152,
+        vbrInfo: {
+          isCBR: true,
+          numberOfFrames: 185,
+        },
+        lameInfo: {
+          encoder: 'LAME3.100',
+          paddingStart: 576,
+          paddingEnd: 1058
+        },
+      });
+    });
+
+    it('sintel-clip-mono-lame-cbr-128.mp3', async () => {
+      const src = path.join(TRACKS_SINTEL, 'sintel-clip-mono-lame-cbr-128.mp3');
+      expect(await getMetadataFromFile(src)).to.deep.equal({
+        version: '1',
+        layer: '3',
+        bitrate: 128,
+        mode: 'mono',
+        sampleRate: 44100,
+        samplesPerFrame: 1152,
+        vbrInfo: {
+          isCBR: true,
+          numberOfFrames: 185,
+        },
+        lameInfo: {
+          encoder: 'LAME3.100',
+          paddingStart: 576,
+          paddingEnd: 1058
+        },
+      });
     });
   });
 
