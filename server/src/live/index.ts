730afb9dcfe8abf3c315331d6681c8c711ff8649
Sam Lanning <sam@samlanning.com>
2023-05-11 19:36:32 +0000

    live: add config loading / saving

--- server/src/live/index.ts
+++ server/src/live/index.ts
@@ -3,4 +3,10 @@ import { VIRTUAL_OUTPUT_PLUGIN } from '@synesthesia-project/live-core/lib/plugin
 import { FILL_INPUT_PLUGIN } from '@synesthesia-project/live-core/lib/plugins/fill-input';
 import { ADD_INPUT_PLUGIN } from '@synesthesia-project/live-core/lib/plugins/add-input';
 
-Stage([VIRTUAL_OUTPUT_PLUGIN, FILL_INPUT_PLUGIN, ADD_INPUT_PLUGIN]);
+const configPath = process.argv[2];
+
+if (!configPath) {
+  throw new Error(`Missing config path argument`);
+}
+
+Stage([VIRTUAL_OUTPUT_PLUGIN, FILL_INPUT_PLUGIN, ADD_INPUT_PLUGIN], configPath);
