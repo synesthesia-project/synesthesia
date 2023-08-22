17dfd366f135e79c23d309791cea67e742ddbd5d
Sam Lanning <sam@samlanning.com>
2023-08-22 19:10:59 +0000

    live-midi: introduce package

---
+++ live/midi/gulpfile.js
@@ -0,0 +1,9 @@
+var util = require('@synesthesia-project/gulp-util');
+var gulp = require('gulp');
+
+util.setupBasicTypescriptProject({
+  clean: ['lib'],
+  outputDir: 'lib'
+});
+
+gulp.task('default', gulp.series('clean', 'ts'));
