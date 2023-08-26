0443945b42b8da6e3d6deb59b32d1c24068fd787
Sam Lanning <sam@samlanning.com>
2023-08-26 15:56:07 +0000

    live-midi: test plugin config

---
+++ live/midi/src/config.ts
@@ -0,0 +1,3 @@
+import * as t from 'io-ts';
+
+export const MIDI_PLUGIN_CONFIG = t.array(t.string);
--- live/midi/src/index.ts
+++ live/midi/src/index.ts
@@ -1,11 +1,20 @@
 import { Plugin } from '@synesthesia-project/live-core/lib/plugins';
 import * as ld from '@synesthesia-project/light-desk';
+import { MIDI_PLUGIN_CONFIG } from './config';
 
 export const MIDI_PLUGIN: Plugin = {
   init: (context) => {
-    context.createTab(
-      'MIDI',
-      new ld.Group({ direction: 'vertical', noBorder: true })
-    );
+    const tab = new ld.Group({ direction: 'vertical', noBorder: true });
+    context.createTab('MIDI', tab);
+
+    const config = context.createConfigSection('midi', MIDI_PLUGIN_CONFIG, []);
+
+    config.addListener((config) => {
+      console.log('midi config changed', config);
+    });
+
+    tab.addChild(new ld.Button('MIDI')).addListener(() => {
+      config.updateConfig((config) => [...config, 'another']);
+    });
   },
 };
