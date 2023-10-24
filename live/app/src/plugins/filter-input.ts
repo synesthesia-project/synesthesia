3b68c77ad922f903daff768ccabc824c780c9a11
Sam Lanning <sam@samlanning.com>
2023-10-24 23:58:49 +0100

    live-app: improve the filter text input updating

--- live/app/src/plugins/filter-input.ts
+++ live/app/src/plugins/filter-input.ts
@@ -6,37 +6,50 @@ import type {
   InputKind,
   Plugin,
 } from '@synesthesia-project/live-core/lib/plugins';
-import { CompositorModule, PixelInfo } from '@synesthesia-project/compositor/lib/modules';
-import { RGBAColor, RGBA_TRANSPARENT } from '@synesthesia-project/compositor/lib/color';
+import {
+  CompositorModule,
+  PixelInfo,
+} from '@synesthesia-project/compositor/lib/modules';
+import {
+  RGBAColor,
+  RGBA_TRANSPARENT,
+} from '@synesthesia-project/compositor/lib/color';
 import {
   OPTIONAL_KIND_AND_CONFIG,
   OptionalKindAndConfig,
 } from '@synesthesia-project/live-core/lib/config';
 import { matches } from 'lodash';
 
-const FILTER_INPUT_CONFIG = t.array(t.type({
-  filter: t.record(t.string, t.string),
-  input: OPTIONAL_KIND_AND_CONFIG
-}));
+const FILTER_INPUT_CONFIG = t.array(
+  t.type({
+    filter: t.record(t.string, t.string),
+    input: OPTIONAL_KIND_AND_CONFIG,
+  })
+);
 
 type Config = t.TypeOf<typeof FILTER_INPUT_CONFIG>;
 
 type Option = {
-  config: Config[number],
-  group: ld.Group,
-  filterGroup: ld.Group,
+  config: Config[number];
   input: Input<OptionalKindAndConfig>;
-}
+  components: {
+    group: ld.Group;
+    filterGroup: ld.Group;
+    filters: Map<string, ld.Group>;
+  };
+};
 
 const getProperties = (info: PixelInfo<unknown>): Record<string, string> => {
   const properties: Record<string, string> = {};
-  for (const [key, value] of Object.entries((info.data as { properties: Record<string, unknown>}).properties)) {
+  for (const [key, value] of Object.entries(
+    (info.data as { properties: Record<string, unknown> }).properties
+  )) {
     if (typeof value === 'string') {
       properties[key] = value;
     }
   }
   return properties;
-}
+};
 
 const createFilterInput = (context: InputContext<Config>): Input<Config> => {
   const group = new ld.Group({ direction: 'vertical', noBorder: true });
@@ -44,10 +57,10 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
   const options: Option[] = [];
 
   const cache: {
-    pixelToOption: Map<PixelInfo<unknown>, Option | 'none'>
+    pixelToOption: Map<PixelInfo<unknown>, Option | 'none'>;
   } = {
-    pixelToOption: new Map()
-  }
+    pixelToOption: new Map(),
+  };
 
   let knownProperties: null | Set<string> = null;
 
@@ -57,14 +70,19 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
     new ld.Button({ text: 'Add Layer', icon: 'add' })
   );
   addLayer.addListener('click', () => {
-    context.updateConfig((current) => [...(current || []), {filter: {}, input: null}]);
+    context.updateConfig((current) => [
+      ...(current || []),
+      { filter: {}, input: null },
+    ]);
   });
 
   // TODO: fire an event when properties are changed,
   // and add a listener to auto refresh properties here when that happens
-  header.addChild(
-    new ld.Button({ text: 'Refresh Properties', icon: 'refresh' })
-  ).addListener('click', () => { knownProperties = null; });
+  header
+    .addChild(new ld.Button({ text: 'Refresh Properties', icon: 'refresh' }))
+    .addListener('click', () => {
+      knownProperties = null;
+    });
 
   const layersGroup = group.addChild(
     new ld.Group({ direction: 'vertical', noBorder: true })
@@ -72,29 +90,51 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
 
   const updateFilters = (oId: number) => {
     const option = options[oId];
-    option.filterGroup.removeAllChildren();
+    option.components.filterGroup.removeAllChildren();
     for (const p of knownProperties || []) {
-      option.filterGroup.addChild(new ld.Label({ text: `${p}:`}));
-      option.filterGroup
-        .addChild(new ld.TextInput({value: option.config.filter[p]}))
-        .addListener('change', (value => context.updateConfig(current => {
-          return [
-            ...current.slice(0, oId),
-            {
-              ...current[oId],
-              filter: value ? {
-                ...current[oId].filter,
-                [p]: value
-              } : Object.fromEntries(Object.entries(current[oId].filter).filter(([k]) => k !== p))
-            },
-            ...current.slice(oId + 1)
-          ]
-        })))
+      // Add new property
+      if (!option.components.filters.has(p)) {
+        const filterGroup = new ld.Group({ noBorder: true});
+        filterGroup.addChild(new ld.Label({ text: `${p}:` }));
+        filterGroup
+          .addChild(new ld.TextInput({ value: option.config.filter[p] }))
+          .addListener('change', (value) =>
+            context.updateConfig((current) => {
+              return [
+                ...current.slice(0, oId),
+                {
+                  ...current[oId],
+                  filter: value
+                    ? {
+                        ...current[oId].filter,
+                        [p]: value,
+                      }
+                    : Object.fromEntries(
+                        Object.entries(current[oId].filter).filter(
+                          ([k]) => k !== p
+                        )
+                      ),
+                },
+                ...current.slice(oId + 1),
+              ];
+            })
+          );
+        option.components.filters.set(p, filterGroup);
+      }
     }
-  }
+    for (const key of option.components.filters.keys()) {
+      if (!knownProperties?.has(key)) {
+        option.components.filters.delete(key);
+      }
+    }
+    option.components.filterGroup.addChildren(
+      ...[...option.components.filters.entries()]
+      .sort(([a], [b]) => a.localeCompare(b))
+      .map(([_, filterGroup]) => filterGroup)
+    );
+  };
 
   const render: CompositorModule['render'] = (map, pixels) => {
-
     // if properties don't exist yet, populate them
     if (!knownProperties) {
       knownProperties = new Set<string>();
@@ -111,7 +151,12 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
     // TODO: make this more efficient by only passing along the filtered pixels
     // to children modules
 
-    const optionToRender = new Map<Option, RGBAColor[]>(options.map(option => [option, option.input.getModlue().render(map, pixels)]));
+    const optionToRender = new Map<Option, RGBAColor[]>(
+      options.map((option) => [
+        option,
+        option.input.getModlue().render(map, pixels),
+      ])
+    );
 
     return pixels.map<RGBAColor>((info, i) => {
       let option = cache.pixelToOption.get(info);
@@ -119,12 +164,12 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
       if (!option) {
         for (const o of options) {
           if (matches(o.config.filter)(getProperties(info))) {
-            cache.pixelToOption.set(info, option = o);
+            cache.pixelToOption.set(info, (option = o));
             break;
           }
         }
         if (!option) {
-          cache.pixelToOption.set(info, option = 'none');
+          cache.pixelToOption.set(info, (option = 'none'));
         }
       }
       // Get render
@@ -135,8 +180,8 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
         }
       }
       return RGBA_TRANSPARENT;
-    })
-  }
+    });
+  };
 
   return {
     applyConfig: (config, lastConfig) => {
@@ -157,7 +202,7 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
 
         group.addHeaderChild(deleteButton);
 
-        const filterGroup = group.addChild(new ld.Group({ noBorder: true}));
+        const filterGroup = group.addChild(new ld.Group({ noBorder: true }));
 
         const input = context.createInputSocket({
           updateConfig: async (update) =>
@@ -165,12 +210,20 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
               ...current.slice(0, i),
               {
                 ...current[i],
-                input: update(current[i].input)
+                input: update(current[i].input),
               },
               ...current.slice(i + 1),
             ]),
         });
-        options[i] = { group, input, filterGroup, config: config[i] };
+        options[i] = {
+          input,
+          config: config[i],
+          components: {
+            group,
+            filterGroup,
+            filters: new Map(),
+          },
+        };
         input.applyConfig(config[i].input, null);
         group.addChild(input.getLightDeskComponent());
         layersGroup.addChild(group);
@@ -178,7 +231,7 @@ const createFilterInput = (context: InputContext<Config>): Input<Config> => {
       // Remove any extra layers
       options.splice(config.length).map((layer) => {
         layer.input.destroy();
-        layersGroup.removeChild(layer.group);
+        layersGroup.removeChild(layer.components.group);
       });
       // Update each layers' config
       for (let i = 0; i < config.length; i++) {
