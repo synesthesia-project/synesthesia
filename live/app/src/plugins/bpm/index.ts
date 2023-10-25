ca809a8cb7218ca3ac6dfe92174232ec38e12620
Sam Lanning <sam@samlanning.com>
2023-10-25 19:44:31 +0100

    live-app: introduce bpm bar size and indexes

--- live/app/src/plugins/bpm/index.ts
+++ live/app/src/plugins/bpm/index.ts
@@ -3,13 +3,26 @@ import * as ld from '@synesthesia-project/light-desk';
 import { createEventEmitter } from '@synesthesia-project/live-core/lib/events';
 import { z } from 'zod';
 import { createAction } from '@synesthesia-project/live-core/lib/actions';
-import { RGBAColor } from '@synesthesia-project/compositor/lib/color';
+import {
+  RGBA_TRANSPARENT,
+  RGBA_WHITE,
+} from '@synesthesia-project/compositor/lib/color';
 
-const BEAT_EVENT_EMITTER = createEventEmitter<{ periodMs: number }>('beat');
+type BeatEvent = {
+  periodMs: number;
+  index: number;
+  barSize: number;
+  /** 0-index id of the beat within the bar */
+  barIndex: number;
+};
+
+const BEAT_EVENT_EMITTER = createEventEmitter<BeatEvent>('beat');
 const STOP_BEAT_ACTION = createAction('beat.stop', z.unknown());
 const RECORD_BEAT_ACTION = createAction('beat.record', z.unknown());
 
 export const BEAT_EVENT = BEAT_EVENT_EMITTER.register;
+
+let barSize = 4;
 
 let manualRecording: null | {
   firstBeat: number;
@@ -17,10 +30,30 @@ let manualRecording: null | {
   commitTimeout: null | NodeJS.Timer;
 } = null;
 
+let lastBeat: null | {
+  index: number;
+} = null;
+
 let beatInterval: null | NodeJS.Timer = null;
 
 const broadcastBeat = (periodMs: number) => {
-  BEAT_EVENT_EMITTER.emit({ periodMs });
+  if (lastBeat) {
+    if (manualRecording) {
+      // Calculate beat ID based on current timing information
+      // (to avoid manual beats advancing id)
+      const timeDiff = Date.now() - manualRecording.firstBeat;
+      lastBeat.index = Math.round(timeDiff / periodMs);
+    } else {
+      lastBeat.index++;
+    }
+    const index = lastBeat.index;
+    BEAT_EVENT_EMITTER.emit({
+      barSize,
+      index,
+      barIndex: index % barSize,
+      periodMs,
+    });
+  }
 };
 
 const recordManualBeat = () => {
@@ -30,6 +63,9 @@ const recordManualBeat = () => {
       firstBeat: Date.now(),
       count: 0,
       commitTimeout: null,
+    };
+    lastBeat = {
+      index: 0,
     };
   } else {
     manualRecording.commitTimeout &&
@@ -55,6 +91,7 @@ const commitManualBeat = () => {
 const stop = () => {
   beatInterval && clearInterval(beatInterval);
   manualRecording = null;
+  lastBeat = null;
 };
 
 STOP_BEAT_ACTION.addListener(stop);
@@ -65,12 +102,27 @@ export const BPM_PLUGIN: Plugin = {
     context.registerAction(STOP_BEAT_ACTION.action);
     context.registerAction(RECORD_BEAT_ACTION.action);
 
-    const group = new ld.Group({ wrap: true });
+    const group = new ld.Group({ wrap: true, title: 'Beat' });
     context.registerDeskComponent(group);
 
-    group.setTitle('Beat');
+    const rectsGroup = group.addChild(new ld.Group({ noBorder: true }));
+    const rects: ld.Rect[] = [];
 
-    const rect = group.addChild(new ld.Rect());
+    const updateRectsFromBarSize = () => {
+      if (rects.length !== barSize) {
+        rectsGroup.removeAllChildren();
+        // Add missing children
+        for (let i = rects.length; i < barSize; i++) {
+          rects.push(new ld.Rect());
+        }
+        // Remove extra children
+        rects.splice(barSize, rects.length - barSize);
+        // Add to light-desk
+        rectsGroup.addChildren(...rects);
+      }
+    };
+
+    updateRectsFromBarSize();
 
     group
       .addChild(new ld.Button({ text: 'Stop' }))
@@ -79,22 +131,25 @@ export const BPM_PLUGIN: Plugin = {
       .addChild(new ld.Button({ text: 'Beat' }))
       .addListener('click', () => RECORD_BEAT_ACTION.action.trigger(null));
 
-    let rectFrameInterval: null | NodeJS.Timer = null;
+    group
+      .addChild(
+        new ld.SliderButton({
+          value: barSize,
+          min: 1,
+          max: 64,
+          step: 1,
+          mode: 'writeBack',
+        })
+      )
+      .addListener('change', (value) => {
+        barSize = value;
+        updateRectsFromBarSize();
+      });
 
-    BEAT_EVENT_EMITTER.register.addEventListener(({ periodMs }) => {
-      rectFrameInterval && clearInterval(rectFrameInterval);
-      const beatStartMs = Date.now();
-
-      const frame = () => {
-        const v = Math.max(0, 1 - (Date.now() - beatStartMs) / periodMs);
-        rect.setColor(new RGBAColor(255 * v, 255 * v, 255 * v, 1));
-        if (v === 0) {
-          rectFrameInterval && clearInterval(rectFrameInterval);
-        }
-      };
-
-      rectFrameInterval = setInterval(frame, 20);
-      frame();
+    BEAT_EVENT_EMITTER.register.addEventListener(({ barIndex }) => {
+      rects.map((r, i) =>
+        r.setColor(i === barIndex ? RGBA_WHITE : RGBA_TRANSPARENT)
+      );
     });
   },
 };
